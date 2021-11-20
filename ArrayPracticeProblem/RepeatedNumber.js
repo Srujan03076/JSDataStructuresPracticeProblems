11917d8a1f089cb503067c4abd80d35a24fddcec
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-20 12:00:36 +0530

    [Srujan]Added code to find repeated digit between Oto100

---
+++ ArrayPracticeProblem/RepeatedNumber.js
@@ -0,0 +1,11 @@
+let numbers = [];
+
+for (var i = 1; i < 100; i++) {
+    var str = i.toString().split('');
+    //console.log(str);
+    if (str[0] == str[1]) {
+        numbers.push(i);
+    }
+}
+
+console.log(numbers);
